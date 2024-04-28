938e574556cd4916095f16025af4feb8c3de4108
Edward Chhun <edwardchhun3@gmail.com>
2024-04-28 06:45:53 -0700

    Update

--- py_server/main.py
+++ py_server/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from detect import detect_labels, detect_logos
+
 app = Flask(__name__)
 
 @app.route('/api/detect', methods=['GET'])
@@ -11,14 +12,37 @@ def image_recognition():
 
     # Process the image using the detect_labels function
     labels = detect_labels(image_file)
-    logos = detect_logos(image_file)
+    # logos = detect_logos(image_file)
     
     # Return the result as JSON
-    return jsonify({'labels_1': detect_labels(image_file),
-                    'logos_1': detect_logos(image_file),
-                    'labels_2': detect_labels(image_file_2),
-                    'logos_2': detect_logos(image_file_2),
+    return jsonify({'labels': labels
                     })
+    
+    
+# To do: Get the intel LLM running in another psh, use this as an end point
+"""
+Example Json Product:
+
+	"messages": [
+        {
+		"from": "human",
+		"content": "What is cnvrg.io?"
+        }, 
+        {
+		"from": "chatbot",
+		"content": "Cnvrg.io is a comprehensive ML/AI platform that provides tools and services to simplify the machine learning and artificial intelligence development process."
+        }
+    ]
+}
+@app.route('/api/llm', methods=['GET'])
+def chatbot():
+    reponse = intel_llm(messages)
+    
+    return jsonify({"response" : response(messages)
+        
+    })
+
+""" 
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
