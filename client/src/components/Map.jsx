95ee00759907f7b4d9cd198eb824bf503c9f58db
sinoconlar <jorgeroman@email.com>
2024-04-28 12:11:36 -0700

    Added markers to Google Maps

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -62,31 +62,69 @@ export class MapContainer extends Component {
       >
         <Marker
           onClick={this.onMarkerClick}
-          name={'Lost Wallet'}
+          name={'I Lost my wallet'}
           description={"Has anyone seen my wallet? Last seen at the ARC men's bathroom. Thank you"}
+          image="public/wallet.jpeg"
           position={{ lat: 38.5428366593408, lng: -121.74967389178279 }}
         />
         <Marker
           onClick={this.onMarkerClick}
           name={'Lost Dog'}
           description={"My dog ran out of the house and got lost. Please report sighting if you see him. Thank you Good Neighbor!"}
+          image="public/dog.jpg"
           position={{ lat: 38.550208066830656, lng: -121.768789526105 }}
         />
         <Marker
           onClick={this.onMarkerClick}
-          name={'Found Iphone'}
+          name={'Found an iPhone'}
           description={"I found an iPhone laying in arroyo field. Hope the owner sees this!"}
+          image="public/cellphone_found.jpg"
           position={{ lat: 38.5548439169783, lng: -121.77285587083493 }}
+        />
+        <Marker
+          onClick={this.onMarkerClick}
+          name={'Found this Arduino Uno'}
+          description={"Contact me, I used it for Hackdavis'24, I'll promise to give it back lols"}
+          image="public/arduino-found.webp"
+          position={{ lat: 38.54185794728171, lng: -121.75967598119885 }} 
+        />
+        <Marker
+          onClick={this.onMarkerClick}
+          name={'I lost my Arduino Uno'}
+          description={"I'm the only one with an Uno at Hackdavis'24, someone help me find it please?"}
+          image="public/arduino.jpg"
+          position={{ lat: 38.54229166400134, lng: -121.75962469629846 }} 
+        />
+        <Marker
+          onClick={this.onMarkerClick}
+          name={'Have anyone seen an iPhone around Arroyo Field?'}
+          description={"I was doing my daily runs and the phone fell out of my pocket out of nowhere..."}
+          image="public/cellphone.jpg"
+          position={{ lat: 38.55519249765446, lng: -121.77734623227184 }}
+        />
+        <Marker
+          onClick={this.onMarkerClick}
+          name={'My son lost his favorite soccer ball'}
+          description={"My two kids were playing soccer and one kicked it way too far, it was by the Union Center at Davis, please help find my son's ball back."}
+          image="public/football.jpg"
+          position={{ lat: 38.54103855634179, lng: -121.76017257231501 }}
+        />
+        <Marker
+          onClick={this.onMarkerClick}
+          name={'Poodle wandering around UC Davis'}
+          description={"Saw this big boy while biking around campus, Mr. Bubbles is his name, contact me 916-776-7788"}
+          image="public/poodle-found.jpg"
+          position={{ lat: 38.54347059612643, lng: -121.74757966637456 }} //, 
         />
         <InfoWindow
           marker={this.state.activeMarker}
           visible={this.state.showingInfoWindow}
           onClose={this.onClose}
         >
-          <div style={{ backgroundColor: 'white', padding: '10px' }}>
+          <div className='popUp' style={{ backgroundColor: 'white', padding: '10px' }}>
             <h4 style={{fontSize: '30px'}}>{this.state.selectedPlace.name}</h4>
             <p style={{fontSize: '15px'}}>{this.state.selectedPlace.description}</p>
-            <img src='' alt='Image' style={{width: '200px'}}></img>
+            <img src={this.state.selectedPlace.image} style={{width: '200px'}} alt="Marker Image" />
           </div>
         </InfoWindow>
       </Map>
