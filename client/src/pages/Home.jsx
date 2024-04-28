5c410626c92ace435f3dcb01b7d36018e0896e73
Juan Santelices <86135408+jdsantelicesl@users.noreply.github.com>
2024-04-27 17:48:33 -0700

    added feed page and home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,13 +3,16 @@ import Map from '../components/Map'
 import PostFeed from '../components/PostFeed'
 
 function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      Home Page
-      <Map/>
-      <PostFeed/>
-    </div>
-  )
+    return (
+        <div className="text-left">
+            <div className="flex flex-col items-center justify-center text-left">
+                <div className="pTitle">
+                    Home
+                </div>
+                {/* add home page content here (intro) */}
+            </div>
+        </div>
+    )
 }
 
 export default Home
