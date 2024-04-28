440a3ff0d8db354376dfa4688f7eb76ead0f71cf
Edward Chhun <edwardchhun3@gmail.com>
2024-04-28 07:09:28 -0700

    Added more content to webpage

--- client/src/components/PostFeed.jsx
+++ client/src/components/PostFeed.jsx
@@ -6,7 +6,7 @@ function PostFeed() {
     <div className=" my-4 flex flex-col justify-center gap-4">
 
       <div className="postBlock">
-        <img src="/wallet.jpeg" className="postImg" />
+        <img src="wallet.jpeg" className="postImg" />
         <div>
           <div className="flex items-center">
             <img src="/pfp.jpeg" className="postPFP"/>
@@ -22,7 +22,7 @@ function PostFeed() {
       </div>
 
       <div className="postBlock">
-        <img src="/phone.webp" className="postImg"/>
+        <img src="/cellphone.jpg" className="postImg"/>
         <div>
           <div className="flex items-center">
             <img src="/pfp.jpeg" className="postPFP" />
@@ -30,8 +30,22 @@ function PostFeed() {
             <div className="postTag" style={{backgroundColor: "rgb(0, 153, 76, 0.6)"}}> Found </div>
           </div>
           <h3 className="postTitle"> Found an iPhone </h3>
-          <div className="postContent"> I found an iPhone laying in arroyo field. Hope the owner sees this! </div>
+          <div className="postContent"> I found an iPhone laying in Arroyo Field. Hope the owner sees this! </div>
         </div>
+      </div>
+
+      <div className="postBlock">
+        <img src="/cellphone_found.jpg" className="postImg"/>
+        <div>
+          <div className="flex items-center">
+            <img src="/pfp.jpeg" className="postPFP" />
+            <div className="postUser"> Sai Tarun </div>
+            <div className="postTag"> Lost </div>
+          </div>
+          <h3 className="postTitle"> Have anyone seen an iPhone around Arroyo Field? </h3>
+          <div className="postContent"> I was doing my daily runs and the phone fell out of my pocket out of nowhere... </div>
+        </div>
+        <div className="postFound"> Report Sighting </div>
       </div>
 
       <div className="postBlock">
@@ -50,6 +64,67 @@ function PostFeed() {
         </div>
       </div>
 
+      <div className="postBlock">
+        <img src="/poodle.jpg" className="postImg" />
+        <div>
+          <div className="flex items-center">
+            <img src="/pfp.jpeg" className="postPFP" />
+            <div className="postUser"> Jacob Johns </div>
+            <div className="postTag"> Lost </div>
+          </div>
+          <h3 className="postTitle"> Lost Dog </h3>
+          <div className="postContent"> I left the backdoor open last night by accident, now Mr.Bubbles is gone! Good Neighbor do your magic! </div>
+          <div className="flex">
+            <div className="postFound"> Report Sighting </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="postBlock">
+        <img src="/poodle-found.jpg" className="postImg" />
+        <div>
+          <div className="flex items-center">
+            <img src="/pfp.jpeg" className="postPFP" />
+            <div className="postUser"> Jason Tatum </div>
+            <div className="postTag" style={{backgroundColor: "rgb(0, 153, 76, 0.6)"}}> Found </div>
+          </div>
+          <h3 className="postTitle"> Poodle wandering around UC Davis </h3>
+          <div className="postContent"> Saw this big boy while biking around campus, Mr. Bubbles is his name, contact me 916-776-7788 </div>
+          <div className="flex">
+          </div>
+        </div>
+      </div>
+
+      <div className="postBlock">
+        <img src="/football.jpg" className="postImg" />
+        <div>
+          <div className="flex items-center">
+            <img src="/pfp.jpeg" className="postPFP" />
+            <div className="postUser"> Angelina Swanson </div>
+            <div className="postTag"> Lost </div>
+          </div>
+          <h3 className="postTitle"> My son lost his favorite soccer ball </h3>
+          <div className="postContent"> My two kids were playing soccer and one kicked it way too far, it was by the Union Center at Davis, please help find my son's ball back. </div>
+          <div className="flex">
+            <div className="postFound"> Report Sighting </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="postBlock">
+        <img src="/football-found.jpg" className="postImg" />
+        <div>
+          <div className="flex items-center">
+            <img src="/pfp.jpeg" className="postPFP" />
+            <div className="postUser"> Joe Jenkins </div>
+            <div className="postTag" style={{backgroundColor: "rgb(0, 153, 76, 0.6)"}}> Found </div>
+          </div>
+          <h3 className="postTitle"> Jabulani Soccer Ball Found! </h3>
+          <div className="postContent"> Saw the cows were kicking this one, someone come pick it back up! </div>
+          <div className="flex">
+          </div>
+        </div>
+      </div>
       
     </div>
   )
