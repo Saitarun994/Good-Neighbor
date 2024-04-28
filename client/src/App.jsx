a407066bb2bd1b45d6d3986f8a57e92bd38181cc
Juan Santelices <86135408+jdsantelicesl@users.noreply.github.com>
2024-04-27 17:47:14 -0700

    added feed page and home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import ProfileDropdown from './components/ProfileDropDown'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
+import Feed from './pages/Feed'
 import Post from './pages/Post'
 import Profile from './pages/Profile'
 import ChatBot from './components/ChatBot'
@@ -18,7 +19,7 @@ function App() {
   return (
     <BrowserRouter>
       {/*Header*/}
-      <div className="bg-white p-4 shadow-md flex justify-between items-center">
+      <div className="bg-white p-4 shadow-md flex justify-between items-center fixed left-0 right-0">
         <div className="flex items-center">
           <ProfileDropdown options={options} />
           <h1 className="text-black font-extrabold ml-2 title">GOOD NEIGHBOR</h1>
@@ -30,9 +31,10 @@ function App() {
       </div>
 
       {/*The page router for our webapp*/}
-      <main>
+      <main class="main">
         <Routes>
           <Route path="/" element={<Home/>}/>
+          <Route path="/feed" element={<Feed/>}/>
           <Route path="/profile/:id" element={<Profile/>}/>
           <Route path="/post/:id" element={<Post/>}/>
         </Routes>
